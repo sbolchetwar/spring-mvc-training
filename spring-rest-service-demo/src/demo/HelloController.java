acb0d70c13e1c0ac222b9bd920f01da7c2153d0b
sandeep <sbolchetwar@live.com>
2019-05-29 10:17:59 +0530

    Spring Restful service demo

---
+++ spring-rest-service-demo/src/demo/HelloController.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@ResponseBody
+	@RequestMapping(value="/hello")
+	public String hello(){
+		return "Welcome to Restful Service...";
+	}
+}
